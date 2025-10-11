29cc2e5b93f34868f31106bcc76a8003eca696fc
wahyusantso <wahyusantozo36@gmail.com>
2025-10-12 00:15:56 +0700

    feat(filesystem): configure file storage

--- config/filesystems.php
+++ config/filesystems.php
@@ -41,7 +41,16 @@ return [
         'public' => [
             'driver' => 'local',
             'root' => storage_path('app/public'),
-            'url' => env('APP_URL').'/storage',
+            'url' => env('APP_URL') . '/storage',
+            'visibility' => 'public',
+            'throw' => false,
+            'report' => false,
+        ],
+
+        'hosting' => [
+            'driver' => 'local',
+            'root' => '/home/u844121767/domains/wahyusantoso.id/public_html/storage',
+            'url' => env('APP_URL') . '/storage',
             'visibility' => 'public',
             'throw' => false,
             'report' => false,
